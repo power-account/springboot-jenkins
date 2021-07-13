2308415142f6570f802278dbc401e3c7232424be
guoyong <guo-yong123@163.com>
2021-07-13 16:27:58 +0800

    Initial commit

---
+++ src/main/java/com/example/jenkins/SpringbootJenkinsApplication.java
@@ -0,0 +1,14 @@
+package com.example.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJenkinsApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringbootJenkinsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/jenkins/controller/JenkinsController.java
@@ -0,0 +1,25 @@
+package com.example.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description: JenkinsCongroller
+ * @author: 279000728@qq.com
+ * @create: 2021-7-13 16:10
+ **/
+
+@RestController
+public class JenkinsController {
+
+
+    @GetMapping("/hello")
+    public String sayHello(String ms )
+
+    {
+        return "hello,自动部署" +ms;
+
+    }
+
+
+}
