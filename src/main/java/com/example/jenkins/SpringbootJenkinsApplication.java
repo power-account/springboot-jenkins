2352cba503e77362b38b35bd824f6f467b02cf8c
guoyong <guo-yong123@163.com>
2021-12-15 16:32:19 +0800

    1111

--- src/main/java/com/example/jenkins/SpringbootJenkinsApplication.java
+++ src/main/java/com/example/jenkins/SpringbootJenkinsApplication.java
@@ -12,6 +12,8 @@ public class SpringbootJenkinsApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(SpringbootJenkinsApplication.class, args);
+		 SpringApplication.run(SpringbootJenkinsApplication.class, args);
+		  SpringApplication.run(SpringbootJenkinsApplication.class, args);
     }
 
 }
